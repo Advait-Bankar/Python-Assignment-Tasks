8270aba0f2e4f23498ae2772dd6fb901ebd7a9ad
Advait-Bankar <advaitbankar45@gmail.com>
2025-06-28 22:58:43 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -18,12 +18,3 @@ print(f"Multiplication: {m}")
 print(f"Division: {d}")
 
 print("\nThank you very much for trying out this program...")
-
-
-# Personalized Greeting Program
-
-print("\nHi Buddy, What's you name?")
-fn = input("Enter your First Name: ")
-sn = input("Enter your Surname: ")
-
-print(f"Hello, {fn} {sn}! Welcome to Python Programming...")
