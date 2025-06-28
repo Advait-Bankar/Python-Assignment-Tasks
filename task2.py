27c90717f41dc32f9fb0c43b7d0c88ba698fe2c5
Advait-Bankar <advaitbankar45@gmail.com>
2025-06-28 22:47:26 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,8 @@
+# Personalized Greeting Program
+
+print("\nHi Buddy, What's your name?")
+fn = input("Enter your First Name: ")
+sn = input("Enter your Surname: ")
+
+print(f"Hello, {fn} {sn}! Welcome to Python Programming...")
+print("\nThanks for using this Programme...")
